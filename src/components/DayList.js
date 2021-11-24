f94b64c6480b573957bcd28104010e0119dca9ea
Andriy Lytvynchuk <andrey@Andron-Mac.hitronhub.home>
2021-11-23 21:04:52 -0500

    addd InterviewerListItem.js

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,12 +5,12 @@ export default function DayList(props){
 
   const printDays =  props.days.map((day) => {
    return <DayListItem 
-          key={day.id}
-          name={day.name} 
-          spots={day.spots} 
-          selected={day.name === props.day}
-          setDay={props.setDay}  
-        />
+            key={day.id}
+            name={day.name} 
+            spots={day.spots} 
+            selected={day.name === props.day}
+            setDay={props.setDay}  
+          />
   });
 
   return(
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classNames from "classnames";
+import '../styles/InterviewerListItem.scss';
+
+export default function InterviewerListItem(props) {
+
+  let interviewersClass = {
+    "interviewers__item": true,
+    "interviewers__item--selected": props.selected ? true : false
+  }
+  return (
+    <li className={classNames(interviewersClass)} onClick={props.setInterviewer} >
+      <img
+        className="interviewers__item-img"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  )
+}
