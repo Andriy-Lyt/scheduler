b5ee156ddaf0265038301a029c93ceb480f0a87a
Andriy Lytvynchuk <andrey@Andron-Mac.hitronhub.home>
2021-11-26 23:05:11 -0500

    show appointment in Appointment.js

--- src/components/Appointment/Index.js
+++ src/components/Appointment/Index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './styles.scss';
+import Header from "./Header"; 
+import Show from "./Show"; 
+import Empty from "./Empty"; 
 
 export default function Appointment(props) {
-  let text = props.time ? "Appoint at 12 pm" : "no appoint";
 
   return(
-    <article className="appointment">{text}</article>
+    <article className="appointment">
+      <Header time={props.time} />
+
+      {props.interview ? 
+        <Show 
+        student={props.interview.student} 
+        interviewer={props.interview.interviewer}
+      /> 
+      :
+      <Empty />
+    }
+    </article>
   )
 }
